29a43268d5f289951c1f96a7b7769b82000ae386
Igor <maildevcom@gmail.com>
2017-08-04 11:58:39 +0300

    simple directive has been added to provide checkbox functionality;

---
+++ frontend/app/components/work-packages/wp-processes/wp-processes.directive.ts
@@ -0,0 +1,126 @@
+// -- copyright
+// OpenProject is a project management system.
+// Copyright (C) 2012-2015 the OpenProject Foundation (OPF)
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License version 3.
+//
+// OpenProject is a fork of ChiliProject, which is a fork of Redmine. The copyright follows:
+// Copyright (C) 2006-2013 Jean-Philippe Lang
+// Copyright (C) 2010-2013 the ChiliProject Team
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+//
+// See doc/COPYRIGHT.rdoc for more details.
+// ++
+
+import {opWorkPackagesModule} from "../../../angular-modules";
+import {scopedObservable} from "../../../helpers/angular-rx-utils";
+import {debugLog} from "../../../helpers/debug_output";
+import {WorkPackageResourceInterface} from "../../api/api-v3/hal-resources/work-package-resource.service";
+import {DisplayField} from "../../wp-display/wp-display-field/wp-display-field.module";
+import {WorkPackageDisplayFieldService} from "../../wp-display/wp-display-field/wp-display-field.service";
+import {WorkPackageEditFormController} from "../../wp-edit/wp-edit-form.directive";
+import {WorkPackageCacheService} from "../work-package-cache.service";
+
+interface FieldDescriptor {
+  name:string;
+  label:string;
+  field?:DisplayField;
+  fields?:DisplayField[];
+  spanAll:boolean;
+  multiple:boolean;
+}
+
+interface GroupDescriptor {
+  name:string;
+  members:FieldDescriptor[];
+}
+
+export class WorkPackageProcessesViewController {
+  public formCtrl: WorkPackageEditFormController;
+  public workPackage: WorkPackageResourceInterface;
+
+  // Grouped fields returned from API
+  public groupedFields:GroupDescriptor[] = [];
+  // Special fields (project, type)
+  public specialFields:FieldDescriptor[];
+  public hideEmptyFields: boolean = true;
+  public text: any;
+  public scope: any;
+  public typesList:any;
+
+  protected firstTimeFocused: boolean = false;
+
+  constructor(protected $http:ng.IHttpService,
+              protected $scope:ng.IScope,
+              protected $rootScope:ng.IRootScopeService,
+              protected $stateParams:ng.ui.IStateParamsService,
+              protected I18n:op.I18n,
+              protected wpDisplayField:WorkPackageDisplayFieldService,
+              protected wpCacheService:WorkPackageCacheService) {
+
+    // Subscribe to work package
+    const workPackageId = this.workPackage ? this.workPackage.id : $stateParams['workPackageId'];
+    scopedObservable(
+      $scope,
+      wpCacheService.loadWorkPackage(workPackageId).values$())
+      .subscribe((wp: WorkPackageResourceInterface) => {
+        this.init(wp);
+      });
+  }
+
+  private init(wp:WorkPackageResourceInterface) {
+    this.workPackage = wp;
+
+    /* TODO: list of types needed to show like checkbox es */
+    this.$http.get('/api/v3/types').then((response: any) => {
+      console.log(response.data._embedded.elements);
+
+      this.text = 'another text';
+      alert('response');
+    })
+  }
+
+}
+
+function wpProcessesDirective() {
+
+  function wpSingleViewLink(scope:ng.IScope,
+                            element:ng.IAugmentedJQuery,
+                            attrs:ng.IAttributes,
+                            controllers: [WorkPackageEditFormController, WorkPackageProcessesViewController]) {
+
+    controllers[1].formCtrl = controllers[0];
+  }
+
+  return {
+    restrict: 'E',
+    templateUrl: '/components/work-packages/wp-processes/wp-processes.directive.html',
+
+    scope: {
+      workPackage: '=?'
+    },
+
+    require: ['^wpEditForm', 'wpProcessesView'],
+    link: wpSingleViewLink,
+
+    bindToController: true,
+    controller: WorkPackageProcessesViewController,
+    controllerAs: '$ctrl'
+  };
+}
+
+opWorkPackagesModule.directive('wpProcessesView', wpProcessesDirective);
